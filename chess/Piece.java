0a90838b2f0c49cedc10aca7726b97dce69f2c86
Wai Yan Aung <WaiYan.Aung84@thehutgroup.com>
2024-04-06 23:23:11 +0100

    Implemented Abstract Class Piece

--- chess/Piece.java
+++ chess/Piece.java
@@ -1,5 +1,20 @@
 package chess;
 
-public class Piece {
-  
+public abstract class Piece {
+  private String symbol;
+  protected PieceColour colour;
+
+  public String getSymbol() {
+    return this.symbol;
+  }
+
+  public void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+
+  public PieceColour getColour() {
+    return this.colour;
+  }
+
+  public  abstract boolean isLegitMove(int i0, int j0, int i1, int j1);
 }
